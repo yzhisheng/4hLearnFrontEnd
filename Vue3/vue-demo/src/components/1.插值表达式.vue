f12fd7e01a320665ce9ae8a3e2d3e8061c40b049
Sakura <1980233736@qq.com>
2024-05-21 11:35:29 +0800

    vue指令

---
+++ Vue3/vue-demo/src/components/1.插值表达式.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+let name = '凹凸曼'
+
+let car = {
+    brand: '小米',
+    version: 'pro max',
+    price: 999
+}
+</script>
+
+
+<template>
+    <h1>插值表达式</h1>
+    <h2>姓名:{{ name }}</h2>
+    <h2>品牌:{{ car.brand }}</h2>
+    <h2>型号:{{ car.version }}</h2>
+    <h2>价格:{{ car.price }}</h2>
+</template>
+
+<style scoped></style>
---
+++ Vue3/vue-demo/src/components/2.指令.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+let name = '凹凸曼'
+
+let car = {
+    brand: '小米',
+    version: 'pro max',
+    price: 800
+}
+
+let msg = `<p style='color: red'> hello world!</p>`
+
+function buy() {
+    alert("已购买!")
+}
+</script>
+
+
+<template>
+    <h1>指令一: v-html,v-text</h1>
+    <div v-html="msg"></div>
+    <div v-text="msg"></div>
+    <!-- //相当于插值表达式 -->
+    <div>{{ msg }}</div>
+
+    <br>
+    <br>
+    <br>
+    <h1>指令二: v-on</h1>
+    <button v-on:click="buy">Buy</button>
+    <!-- 简写形式 -->
+    <button @click="buy">购买</button>
+
+    <br>
+    <br>
+    <br>
+    <h1>指令三: v-if 和 v-for</h1>
+    <span style="color: red" v-if="car.price > 900">雷子,你卖贵了</span>
+    <span style="color: green" v-if="car.price <= 900">雷总,你是我滴神</span>
+</template>
+
+<style scoped></style>
